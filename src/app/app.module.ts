4e02f6b6984c33a98a05b2881ffa06d0225645bc
Brian Mwiruki <mwiruki@outlook.com>
2020-01-20 12:38:41 +0300

    home.pipe initialization in the root directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HomePipe } from './home.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    HomePipe
    ],
   imports: [
     BrowserModule,
