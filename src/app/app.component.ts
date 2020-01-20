07ce21cdc057f60e6b5899f705ef844bcf65bb01
Brian Mwiruki <mwiruki@outlook.com>
2020-01-20 15:51:19 +0300

    userpage component, profileService method initialization and subscribe  method implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,10 @@
-import { ProfileService } from './profile.service';
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public username: string;
-  public Repositories: string;
-  public followers: string;
-  public following: string;
-  public avatar_url: string;
-  public email: string;
-  public created_at: Date;
-  repos: Object;
-  constructor(private _githubService: ProfileService) {
-
-  }
-
-  search() {
-    this._githubService.updateUsername(this.username);
-
-    this._githubService.getProfileInfo().subscribe(user => {
- 
-      this.username = this.username;
-    });
-
-    this._githubService.getRepos().subscribe(repos => {
-      this.repos = repos;
-    });
-  }
+  title = 'GitSearch';
 }
--- src/app/userpage/userpage.component.ts
+++ src/app/userpage/userpage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService }from '../profile.service';
+
 
 @Component({
   selector: 'app-userpage',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserpageComponent implements OnInit {
 
-  constructor() { }
+  profile;
+  repos;
+  username: string;
+
+  constructor(private profileService:ProfileService) {
+
+
+    searchProfile(){
+
+    this.profileService.updateProfile(this.username);
+    this.profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
+    }
+
+    // this.profileService.getProfileInfo().subscribe(profile => this.profile = profile);
+    // this.profileService.getProfileRepos().subscribe(repos =>  this.repos = repos);
+
+
 
   ngOnInit() {
+
+    this.profileService.updateProfile("Bryan4real");
+    this.profileService.getProfileInfo().subscribe(profile => this.profile = profile);
+    this.profileService.getProfileRepos().subscribe(repos =>  this.repos = repos);
+
   }
 
 }
